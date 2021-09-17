2f40d74bb03ba8b9b9d79a10bd9bc811aa37cbd8
arnaudlabourel <arnaud.labourel@univ-amu.fr>
2021-09-17 16:43:31 +0200

    Ajout import dans Display.java

--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -1,13 +1,12 @@
 package viewer;
 
-import image.BlankImageFactory;
-import image.Image;
-import image.ImageFactory;
+import image.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelWriter;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
